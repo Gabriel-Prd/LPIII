3702a6cf727461edc6559f25f3968338b7e76368
Gabriel-Prd <gabrielpradomartinsgbi@gmail.com>
2023-09-06 15:39:57 -0300

    Questoes feitas

--- Lista 1/q1.java
+++ Lista 1/q1.java
@@ -1,3 +1,39 @@
+import java.util.Scanner;
+
 public class q1 {
-    
+    public static void main(String[] args){
+        double sum = 0, variacao = 0;
+        final double [] medicao = new double[10];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i = 0; i < medicao.length; i++){
+            
+            System.out.printf("Medicao %d: ", i);
+            double medido = scanner.nextDouble();
+
+            medicao[i] = medido;
+            sum += medido;
+        }
+
+        double media = sum/medicao.length;
+
+        for(int i = 0; i < medicao.length; i++){
+            variacao += Math.pow(medicao[i] - media, 2);
+            variacao /= medicao.length; 
+        }
+
+        double desvio = Math.sqrt(variacao);
+
+        System.out.println("Variacao: " + variacao);
+        System.out.println("Desvio padrao: " + desvio);
+
+        if(media * 0.1 < desvio){
+            System.out.println("O multimetro esta com problemas.");
+        }else{
+            System.out.println("O multimetro esta funcionando.");
+        }
+        
+
+    }
 }
--- Lista 1/q2.java
+++ Lista 1/q2.java
@@ -1,3 +1,45 @@
 public class q2 {
-    
+    public static void main(String[] args){
+        int dado1 = (int) (Math.random() * 6) + 1;
+        int dado2 = (int) (Math.random() * 6) + 1;
+        int pontos = dado1 + dado2;
+        int contador = 0;
+
+
+        System.out.printf("Dado 1 = %d, Dado 2 = %d\n", dado1, dado2);
+        System.out.printf("Total de pontos = %d\n", pontos);
+
+        if(pontos == 7 || pontos == 11){
+            System.out.println("Voce Ganhou!!");
+            return;
+        }
+
+        if(pontos == 2 || pontos == 3|| pontos == 12){
+            System.out.println("Voce Perdeu!!");
+            return;
+        }
+
+        System.out.println("Segunda fase!");
+
+        while(true){
+            contador ++;
+            dado1 = (int) (Math.random() * 6) + 1;
+            dado2 = (int) (Math.random() * 6) + 1;
+            int pontos2 = dado1 + dado2;
+
+            System.out.printf("Dado 1 = %d, Dado 2 = %d\n", dado1, dado2);
+            System.out.printf("Jogada %d: %d\n", contador, pontos2);
+
+            if(pontos2 == 7){
+                System.out.println("Voce Perdeu!!");
+                return;
+            }
+
+            if(pontos2 == pontos){
+                System.out.println("Voce Ganhou!!");
+                return;
+            }
+        }
+
+    }
 }
